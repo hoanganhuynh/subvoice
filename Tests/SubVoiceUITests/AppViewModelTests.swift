b80ccf00585460b366d63a200e06d4c41bc47953
Anthony <creative@williens.com>
2026-09-01 21:44:15 +0700

    test: cover settings, preview and recovery intents

--- Tests/SubVoiceUITests/AppViewModelTests.swift
+++ Tests/SubVoiceUITests/AppViewModelTests.swift
@@ -32,4 +32,18 @@ struct AppViewModelTests {
             .changeRate(0.625), .changeVolume(0.75),
         ])
     }
+
+    @Test func settingsIntentsRemainExplicit() {
+        let model = AppViewModel(state: AppViewState())
+        var received: [AppIntent] = []
+        model.onIntent = { received.append($0) }
+        model.send(.previewVoice)
+        model.send(.setTheme(.dark))
+        model.send(.setLaunchAtLogin(true))
+        model.send(.recover(.openScreenRecordingSettings))
+        #expect(received == [
+            .previewVoice, .setTheme(.dark), .setLaunchAtLogin(true),
+            .recover(.openScreenRecordingSettings),
+        ])
+    }
 }
